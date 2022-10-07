79b4bc596028b9a85817618ce4ffd53c6520bc0b
Mac <mac@MacdeMacBook-Air.local>
2022-10-07 18:37:13 +0800

    initial commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,11 +3,24 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
 
-contract Contract is ERC1155LazyMint {
+contract Evolve is ERC1155LazyMint {
     constructor(
         string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    ) ERC1155LazyMint(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+        string memory _symbol
+    ) ERC1155LazyMint(_name, _symbol, msg.sender, 0) {}
+
+    function verifyClaim(
+        address _claimer,
+        uint256 _tokenId,
+        uint256 _quantity
+    ) public view override {
+        require(_tokenId == 0, "only bada is claimable");
+        require(_quantity == 1, "only one nft can be claimed at a time");
+    }
+
+    function evolve() public {
+        _burn(msg.sender, 0, 2);
+        _mint(msg.sender, 1, 1, "");
+    }
+
 }
